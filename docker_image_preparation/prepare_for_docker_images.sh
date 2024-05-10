4c7e5fde05ed8eda5d871e397106c1a29c3464cb
workstation <isctae@gmail.com>
2024-05-10 17:06:30 -0500

    uncomment the call to prepare for Docker images

--- docker_image_preparation/prepare_for_docker_images.sh
+++ docker_image_preparation/prepare_for_docker_images.sh
@@ -10,19 +10,16 @@ echo current path:
 pwd
 
 echo prepare Docker image for Mythril
-#./mythril/create_mythril_docker_image.sh
+./docker_image_preparation/mythril/create_mythril_docker_image.sh
 
-echo prepare Docker image for Manticore
-#./manticore/create_manticore_docker_image.sh
+#echo prepare Docker image for SmartExecutor
+./docker_image_preparation/smartExecutor/create_smartExecutor_docker_image.sh
 
-echo prepare Docker image for SmartExecutor
-#./smartExecutor/create_smartExecutor_docker_image.sh
-
-echo prepare Docker image for Smartian
-#./smartian/create_smartian_docker_image.sh
+#echo prepare Docker image for Smartian
+./docker_image_preparation/smartian/create_smartian_docker_image.sh
 
 # pull the image used to compile contracts
-# docker pull 23278942/compile1
+docker pull 23278942/compile1
 
 
 
